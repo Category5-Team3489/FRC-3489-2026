dbdb59a146f19c7066cf97a9d4b4cf4f1cf1270e
gunderbus <cole.gundermann@gmail.com>
2026-01-30 19:24:33 -0500

    edit void function

--- src/main/java/frc/robot/subsystems/shooter/shooterIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/shooterIOTalonFX.java
@@ -1,21 +1,19 @@
-package frc.robot.subsystems.shooter;
-
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.math.MathUtil;
 
 package frc.robot.subsystems.shooter;
+
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class shooterIOTalonFX implements shooterIO {
     // Create motors
     private final TalonFX angleMotor;
     private final TalonFX shooterMotor;
 
+    public shooterIOTalonFX(int shooterMotorPort, int angleMotorPort){
+        angleMotor = new TalonFX(angleMotorPort);
+        shooterMotor = new TalonFX(shooterMotorPort);
+    }
+    
     @Override
     public void updateInputs(shooterIOInputs inputs) {
         // TODO Auto-generated method stub
@@ -32,19 +30,22 @@ public class shooterIOTalonFX implements shooterIO {
     @Override
     public void stopMotors() {
         // TODO Auto-generated method stub
-        
-    }
+        shooterMotor.set(0);
+        angleMotor.set(0);
+    }  
 
     @Override
     public void shootBall(double speed) {
-        // TODO Auto-generated method stub
-        
+        // Check this code fs
+        shooterMotor.set(speed);
     }
 
     @Override
-    public void setShootAngle(double angle) {
-        // TODO Auto-generated method stub
-        
+    public void setShootAngle(double degrees) {
+        // Check this code fs
+        double rotations = degrees / 360.0;
+        PositionDutyCycle request = new PositionDutyCycle(rotations);
+        angleMotor.setControl(request);
     }
 
 }
