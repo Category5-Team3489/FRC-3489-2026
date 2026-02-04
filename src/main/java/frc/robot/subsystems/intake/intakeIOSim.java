1d9f41187a1eae6d4848649041528203824fbfc1
gunderbus <cole.gundermann@gmail.com>
2026-02-03 19:16:05 -0500

    made intakeIOSIm

---
+++ src/main/java/frc/robot/subsystems/intake/intakeIOSim.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+
+public class intakeIOSim implements intakeIO {
+    private intakeIOInputs inputs = new intakeIOInputs();
+    private final DCMotor motor = DCMotor.getCIM(1);
+
+    // idek what to do bro
+    
+}
