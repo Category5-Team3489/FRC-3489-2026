48afab1fb8c767ea8d99a2758057b6c33c384291
gunderbus <cole.gundermann@gmail.com>
2026-02-05 22:38:16 +0000

    Programmed the intakeIOSim stuff may need to add some different things to the talonFX one and the intakeIOSim one

--- src/main/java/frc/robot/subsystems/intake/intakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/intakeIOSim.java
@@ -1,11 +1,22 @@
-// package frc.robot.subsystems.intake;
+package frc.robot.subsystems.intake;
 
-// import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.DCMotor;
 
-// public class intakeIOSim implements intakeIO {
-//     private intakeIOInputs inputs = new intakeIOInputs();
-//     private final DCMotor motor = DCMotor.getCIM(1);
+public class intakeIOSim implements intakeIO {
+    private intakeIOInputs inputs = new intakeIOInputs();
+    private final DCMotor motor = DCMotor.getCIM(1);
 
-//     // idek what to do bro
+    // idek what to do bro
 
-// }
+    // This is the spin that stuff void
+    @Override
+    public void spinThatStuff(double initialSpeed){
+        motor.setInputVoltage(initialSpeed);
+    }
+
+    @Override
+    public void stopMotors(){
+        motor.setInputVoltage(0);
+    }
+
+}
