4928f686de7588e3539bcd4404007df9475b4246
gunderbus <cole.gundermann@gmail.com>
2026-02-05 18:16:58 -0500

    why are there so many bugs

--- src/main/java/frc/robot/subsystems/intake/intakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/intakeIOSim.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems.intake;
 
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
 
 public class intakeIOSim implements intakeIO {
   private intakeIOInputs inputs = new intakeIOInputs();
-  private final DCMotor motor = DCMotor.getCIM(1);
+  private final DCMotorSim motor = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(DCMotor.getCIM(1), 1.0, 0.02), DCMotor.getCIM(1));
 
   // idek what to do bro
 
