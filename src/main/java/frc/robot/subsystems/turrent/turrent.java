d6748788731b592bfbdbe744e3a52eaa74d9498f
gunderbus <cole.gundermann@gmail.com>
2026-02-07 09:28:16 -0500

    added turrent.java

---
+++ src/main/java/frc/robot/subsystems/turrent/turrent.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.turrent;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class turrent extends SubsystemBase{
+    private turrentIO io;
+    
+    public turrent(turrentIO given) {
+        io = given;
+    }
+    
+    public void setTurrentAngle(double degrees) {
+        io.setTurrentAngle(degrees);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/turrent/turrentIO.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.turrent;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface turrentIO {
+    @AutoLog
+    public class turrentIOInputs {
+        public double topMotorCurrent = 0.0;
+        public double bottomMotorCurrent = 0.0;
+        public double turrentAngle = 0.0;
+        public double distanceToTarget = 0.0;
+    }
+
+    // Update inputs
+    public void updateInputs(turrentIOInputs inputs);
+
+    // Set motor to angle
+    public void setTurrentAngle(double degrees);
+}
