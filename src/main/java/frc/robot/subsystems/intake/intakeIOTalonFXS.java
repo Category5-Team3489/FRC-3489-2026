b4bbd972289e8d4577b2dce2211a94e2746269d9
gunderbus <cole.gundermann@gmail.com>
2026-02-07 10:22:42 -0500

    added intakeIOTalonFXS

---
+++ src/main/java/frc/robot/subsystems/intake/intakeIOTalonFXS.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.intake;
+
+public class intakeIOTalonFXS implements intakeIO {
+  // Create motors
+  private final com.ctre.phoenix6.hardware.TalonFXS intakeMotor;
+
+  public intakeIOTalonFXS(int intakeMotorPort) {
+    intakeMotor = new com.ctre.phoenix6.hardware.TalonFXS(intakeMotorPort);
+  }
+
+  @Override
+  public void spinThatStuff(double initialSpeed) {
+    intakeMotor.set(initialSpeed);
+  }
+
+  @Override
+  public void stopMotors() {
+    intakeMotor.set(0);
+  }
+}
