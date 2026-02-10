742c845ecfc9085d4faa729ac89fc35965e1175a
gunderbus <cole.gundermann@gmail.com>
2026-02-09 19:18:38 -0500

    command did it

--- src/main/java/frc/robot/subsystems/turrent/turrent.java
+++ src/main/java/frc/robot/subsystems/turrent/turrent.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.turrent;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionIO;
@@ -13,9 +14,7 @@ public class turrent extends SubsystemBase {
     io = given;
   }
 
-  public Command lockToTarget(Vision eyes, VisionIO eyesio){
-    
-  }
+  
 
   @Override
   public void periodic() {
@@ -26,5 +25,9 @@ public class turrent extends SubsystemBase {
 
   public void setTurrentAngle(double degrees) {
     io.setTurrentAngle(degrees);
+  }
+
+  public Command lockToTarget(Vision eyes, int cameraIndex){
+    return Commands.run(() -> setTurrentAngle(eyes.getTargetX(cameraIndex)));
   }
 }
