fc1322cacce4f0d82295063b09e3f8fa73e3ecde
gunderbus <cole.gundermann@gmail.com>
2026-03-10 19:54:54 -0400

    i need to do this

---
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOInputsAutoLogged.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.drive;
+
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public class ModuleIOInputsAutoLogged extends ModuleIO.ModuleIOInputs implements LoggableInputs {
+  @Override
+  public void toLog(LogTable table) {}
+
+  @Override
+  public void fromLog(LogTable table) {}
+}
